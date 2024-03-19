429d9814499dd7afadcb4d2aad0b987691ae90cc
Lucas Chipolari <zlucaschipolariz@gmail.com>
2024-03-19 19:28:47 -0300

    realice el ejercicio 5

---
+++ eje5.js
@@ -0,0 +1,7 @@
+/*5.- Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+Nota: Tener en cuenta la siguiente función: parseInt
+*/
+
+let primerNumero=parseInt(prompt('Ingrese un numero'));
+let segundoNumero=parseInt(prompt('Ingrese otro número'));
+document.write('La suma de ambos numeros es: ' + (primerNumero + segundoNumero));
