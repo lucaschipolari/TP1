aaea6c77468c32811f1f83eede11f115666b1db3
Lucas Chipolari <zlucaschipolariz@gmail.com>
2024-03-19 19:31:51 -0300

    mejore el ejercio 6

--- eje6.js
+++ eje6.js
@@ -4,14 +4,25 @@ Ejemplo:
 input: 15 , 3
 */
 
-let primerNumero=Number(prompt('Ingrese un numero'));
-let secondNumber=Number(prompt('Ingrese otro numero'));
-if(primerNumero>secondNumber){
-    document.write(primerNumero + ' es el numero mas grande');
-}
-else if(secondNumber>primerNumero){
-    document.write(secondNumber + ' es el numero mas grande');
-}
-else{
-    document.write('Los numeros son iguales');
+let input = prompt('Ingrese el primer número');
+let input2 = prompt('Ingrese un segundo numero');
+
+let numero = input !== '' && !isNaN(input) && parseInt(input) ? Number(input) : null;
+let numero2 = input2 !== '' && !isNaN(input) && parseInt(input) ? Number(input2) : null;
+
+if (numero !== null) {
+    if (numero2 !== null) {
+
+        if (numero > numero2) {
+            alert(numero);
+        }else{
+            alert(numero2);
+        }
+
+    }else{
+        alert('el segundo ingreso es invalido');
+    }
+    
+}else{
+    alert('el primer ingreso es invalido');
 }
